8ea16ea9f3811a87d1a4a5f52f239bf4f9085e87
Major Boner <khodukin88@gmail.com>
2023-11-25 00:07:14 +0700

    add: links to navbar

--- src/widgets/navigation/ui/NavBar/NavBar.tsx
+++ src/widgets/navigation/ui/NavBar/NavBar.tsx
@@ -1,6 +1,9 @@
 import { memo } from 'react';
 import cls from './NavBar.module.scss';
 import { classNames } from '@/shared/lib/helpers/classNames/classNames';
+import { Link } from 'atomic-router-react';
+import { aboutRoute } from '@/pages/about';
+import { mainRoute } from '@/pages/main';
 
 interface NavBarProps {
   className?: string;
@@ -8,5 +11,10 @@ interface NavBarProps {
 
 export const NavBar = memo(function NavBar(props: NavBarProps) {
   const { className } = props;
-  return <div className={classNames(cls.NavBar, {}, [className])}>NavBar</div>;
+  return (
+    <div className={classNames(cls.NavBar, {}, [className])}>
+      <Link to={mainRoute}>Main Page</Link>
+      <Link to={aboutRoute}>About Page</Link>
+    </div>
+  );
 });
