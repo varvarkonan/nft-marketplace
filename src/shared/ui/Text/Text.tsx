9db01381a682d0c0c814c72f7641c6b148016b02
Major Boner <khodukin88@gmail.com>
2023-12-03 18:25:05 +0700

    add: text-align props

--- src/shared/ui/Text/Text.tsx
+++ src/shared/ui/Text/Text.tsx
@@ -5,17 +5,19 @@ import { classNames } from '@/shared/lib/helpers/classNames/classNames';
 type TextFontFamily = 'work' | 'space';
 type TextSize = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'base' | 'caption';
 type TextColor = 'white' | 'gray';
+type TextAlign = 'left' | 'center' | 'right';
 
 interface TextProps {
   className?: string;
   family?: TextFontFamily;
   size?: TextSize;
   color?: TextColor;
+  align?: TextAlign;
   text: string;
 }
 
 export const Text = memo(function Text(props: TextProps) {
-  const { className, family = 'work', size = 'base', color = 'white', text } = props;
-  const additionals = [className, cls[family], cls[size], cls[color]];
+  const { className, family = 'work', size = 'base', color = 'white', align = 'left', text } = props;
+  const additionals = [className, cls[family], cls[size], cls[color], cls[align]];
   return <p className={classNames(cls.Text, {}, additionals)}>{text}</p>;
 });
